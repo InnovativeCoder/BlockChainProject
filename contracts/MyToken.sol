1a0e57e51d1e83d34e92c6a73644a8c3f7993082
InnovativeCoder <aashitsingh2010@gmail.com>
2022-04-22 20:57:52 -0400

    major bug fixes

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply)
+    constructor(uint256 initialnumberoftokens)
         public
         ERC20("Dummy  Token", "DUMM")
     {
-        _mint(msg.sender, initialSupply);
+        _mint(msg.sender, initialnumberoftokens);
     }
 }
--- contracts/MyTokenSale.sol
+++ contracts/MyTokenSale.sol
@@ -1,26 +1,26 @@
 pragma solidity ^0.6.0;
 
 import "./Crowdsale.sol";
-import "./KycContract.sol";
+import "./Kyc_Contract.sol";
 
 contract MyTokenSale is Crowdsale {
     KycContract kyc;
 
     constructor(
-        uint256 rate, // rate in TKNbits
-        address payable wallet,
+        uint256 rate,
+        address payable mywallet,
         IERC20 token,
         KycContract _kyc
-    ) public Crowdsale(rate, wallet, token) {
+    ) public Crowdsale(rate, mywallet, token) {
         kyc = _kyc;
     }
 
-    function _preValidatePurchase(address beneficiary, uint256 weiAmount)
+    function _preValidatePurchase(address beneficiary, uint256 Amountinwei)
         internal
         view
         override
     {
-        super._preValidatePurchase(beneficiary, weiAmount);
+        super._preValidatePurchase(beneficiary, Amountinwei);
         require(
             kyc.kycCompleted(msg.sender),
             "KYC Not completed, purchase not allowed"
